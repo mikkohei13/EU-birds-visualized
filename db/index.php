1c7ddbf95d955c58c388b50590548cfa1951f9c0
Mikko Heikkinen <hawk@biomi.org>
2015-02-21 22:47:04 +0200

    Population data

--- db/index.php
+++ db/index.php
@@ -9,11 +9,28 @@ $db = new Db();
 $species = $db -> quote($_GET['species']);
 $species = str_replace("%20", " ", $species);
 
-$sql = "SELECT * FROM eub_birds WHERE speciesname_cleaned LIKE $species";
-//echo $sql;
-$rows = $db -> select($sql);
+$typeDirty = $_GET['type'];
+$data = Array();
 
-$json = json_encode($rows);
+if ("population" == $typeDirty)
+{
+	$sql = "SELECT country, population_average_size FROM eub_birds WHERE speciesname_cleaned LIKE $species";
+	$rows = $db -> select($sql);
+	foreach ($rows as $key => $arr)
+	{
+		$data[$arr['country']] = $arr['population_average_size'];
+	}
+}
+else
+{
+	$sql = "SELECT * FROM eub_birds WHERE speciesname_cleaned LIKE $species";
+	$rows = $db -> select($sql);
+	$data = $rows;
+}
+
+//echo $sql;
+
+$json = json_encode($data);
 echo $json;
 
 //print_r ($rows);
