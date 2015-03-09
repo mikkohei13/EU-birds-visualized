979babd761535e054dc4343de77d93f6ddfa88ca
Mikko Heikkinen <hawk@biomi.org>
2015-03-09 22:08:26 +0200

    Analytics

--- index.php
+++ index.php
@@ -79,15 +79,7 @@ else
           <a href="https://github.com/mikkohei13/EU-birds-visualized">Source on Github</a>
         </div>
 
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+        <?php include("ja/analytics.php"); ?>
     </body>
 </html>
 <link href='http://fonts.googleapis.com/css?family=Istok+Web:400,700,400italic' rel='stylesheet' type='text/css'>
