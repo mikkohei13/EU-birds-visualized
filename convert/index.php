149e042b21723fec253c7e2eabdd773411460f9b
Mikko Heikkinen <hawk@biomi.org>
2015-03-19 12:54:59 +0200

    Parse all files in one run, print as CSV

--- convert/index.php
+++ convert/index.php
@@ -9,69 +9,121 @@ helpdesk@eionet.europa.eu
 
 require_once 'convert_fields.php';
 
+$headersPrinted = FALSE;
+
 echo "<pre>";
 
-$xml = simplexml_load_file("data/DE_birds_reports.xml");
-
-// Create data
-$i = 0;
-foreach($xml->bird_report as $bird)
+$files = glob('data/*.{xml}', GLOB_BRACE);
+foreach($files as $filename)
 {
-	foreach ($bird->children() as $value)
+  parseFile($filename);
+
+  break; // debug
+}
+
+function parseFile($filename)
+{
+	global $fields;
+
+	$xml = simplexml_load_file($filename);
+
+	// Create data
+	$i = 0;
+	foreach($xml->bird_report as $bird)
 	{
-	    $name = $value->getName(); 
-
-	    if (! $fields[$name])
-	    {
-	    	continue;
-	    }
-
-		$name = trim((string) $name);
-		$value = trim((string) $value);
-
-		if (isset($_GET['nullify']))
+		foreach ($bird->children() as $value)
 		{
-			if (empty($value))
+		    $name = $value->getName(); 
+
+		    if (! $fields[$name])
+		    {
+		    	continue;
+		    }
+
+			$name = trim((string) $name);
+			$value = trim((string) $value);
+
+			if (isset($_GET['nullify']))
 			{
-				$value = "null";
+				if (empty($value))
+				{
+					$value = "null";
+				}
 			}
+			$data[$i][$name] = $value;
+
 		}
-		$data[$i][$name] = $value;
 
+		$data[$i]['speciesname_cleaned'] = returnSpeciesName($data[$i]['speciesname']);
+
+		$data[$i]['population_average_size'] = ($data[$i]['population_minimum_size'] + $data[$i]['population_maximum_size']) / 2;
+		$data[$i]['population_trend_magnitude_average'] = ($data[$i]['population_trend_magnitude_min'] + $data[$i]['population_trend_magnitude_max']) / 2;
+		$data[$i]['population_trend_long_magnitude_average'] = ($data[$i]['population_trend_long_magnitude_min'] + $data[$i]['population_trend_long_magnitude_max']) / 2;
+		$data[$i]['range_trend_magnitude_average'] = ($data[$i]['range_trend_magnitude_min'] + $data[$i]['range_trend_magnitude_max']) / 2;
+		$data[$i]['range_trend_long_magnitude_average'] = ($data[$i]['range_trend_long_magnitude_min'] + $data[$i]['range_trend_long_magnitude_max']) / 2;
+		$data[$i]['spa_population_average'] = ($data[$i]['spa_population_min'] + $data[$i]['spa_population_max']) / 2;
+
+		$i++;
 	}
 
-	$data[$i]['speciesname_cleaned'] = returnSpeciesName($data[$i]['speciesname']);
+	print_r ($data); // exit(); // debug
 
-	$data[$i]['population_average_size'] = ($data[$i]['population_minimum_size'] + $data[$i]['population_maximum_size']) / 2;
-	$data[$i]['population_trend_magnitude_average'] = ($data[$i]['population_trend_magnitude_min'] + $data[$i]['population_trend_magnitude_max']) / 2;
-	$data[$i]['population_trend_long_magnitude_average'] = ($data[$i]['population_trend_long_magnitude_min'] + $data[$i]['population_trend_long_magnitude_max']) / 2;
-	$data[$i]['range_trend_magnitude_average'] = ($data[$i]['range_trend_magnitude_min'] + $data[$i]['range_trend_magnitude_max']) / 2;
-	$data[$i]['range_trend_long_magnitude_average'] = ($data[$i]['range_trend_long_magnitude_min'] + $data[$i]['range_trend_long_magnitude_max']) / 2;
-	$data[$i]['spa_population_average'] = ($data[$i]['spa_population_min'] + $data[$i]['spa_population_max']) / 2;
+//	createInserts($data);
+	createCSV($data);
 
-	$i++;
 }
 
-print_r ($data); // exit(); // debug
-
-// Create SQL INSERT
-foreach ($data as $no => $speciesData)
+function createCSV($data)
 {
-//	echo $speciesData['speciesname'] . "\n";
+	global $headersPrinted;
 
-	$keys = "";
-	$values = "";
-	foreach ($speciesData as $key => $value)
+	// Create SQL INSERT
+	foreach ($data as $no => $speciesData)
 	{
-		$keys = $keys . ", " . mysql_escape_string($key);
-		$values = $values . ", '" . mysql_escape_string($value) . "'";
+	//	echo $speciesData['speciesname'] . "\n";
+		if ($headersPrinted === FALSE)
+		{
+			foreach ($speciesData as $key => $value)
+			{
+				echo mysql_escape_string($key);
+				echo "\t";
+			}
+			echo "\n";
+			$headersPrinted = TRUE;
+		}
+
+		foreach ($speciesData as $key => $value)
+		{
+			echo mysql_escape_string($value);
+			echo "\t";
+		}
+		echo "\n";
 	}
-	$keys = trim($keys, ", ");
-	$values = trim($values, ", ");	
-	echo "INSERT INTO eub_birds ($keys) VALUES ($values);\n";
+
+	//echo "\n\nend";
 }
 
-//echo "\n\nend";
+function createInserts($data)
+{
+	// Create SQL INSERT
+	foreach ($data as $no => $speciesData)
+	{
+	//	echo $speciesData['speciesname'] . "\n";
+
+		$keys = "";
+		$values = "";
+		foreach ($speciesData as $key => $value)
+		{
+			$keys = $keys . ", " . mysql_escape_string($key);
+			$values = $values . ", '" . mysql_escape_string($value) . "'";
+		}
+		$keys = trim($keys, ", ");
+		$values = trim($values, ", ");	
+		echo "INSERT INTO eub_birds ($keys) VALUES ($values);\n";
+	}
+
+	//echo "\n\nend";
+}
 
 function returnSpeciesName($taxon)
 {
